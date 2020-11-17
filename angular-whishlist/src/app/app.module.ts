7e069e61e165eb405347cde3788b10f078a0fee3
Luis Munoz <lmunoz.xyz@gmail.com>
2020-11-16 19:17:00 -0600

    Se agrego la primera parte del Core de Redux y todo bien, instalamos NGRX store y efffects

--- angular-whishlist/src/app/app.module.ts
+++ angular-whishlist/src/app/app.module.ts
@@ -3,18 +3,41 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
+import { StoreModule as NgRxStoreModule, ActionReducerMap } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+
 import { AppComponent } from './app.component';
 import { DestinoViajeComponent } from './destino-viaje/destino-viaje.component';
 import { ListaDestinosComponent } from './lista-destinos/lista-destinos.component';
 import { DestinoDetalleComponent } from './destino-detalle/destino-detalle.component';
 import { DestinosApiClient } from './models/destinos-api-client.model';
 import { FormDestinoViajeComponent } from './form-destino-viaje/form-destino-viaje.component';
+import {
+          DestinosViajesState,
+          intializeDestinosViajesState,
+          reducerDestinosViajes,
+          DestinosViajesEffects
+        } from './models/destinos-viajes-state.model';
 
 const routes: Routes = [
     { path: '', redirectTo: 'home', pathMatch: 'full' },
     { path: 'home', component: ListaDestinosComponent },
     { path: 'destino/:id', component: DestinoDetalleComponent }
   ];
+
+//redux init
+export interface AppState {
+  destinos: DestinosViajesState;
+};
+
+const reducers: ActionReducerMap<AppState> = {
+  destinos: reducerDestinosViajes
+};
+
+let reducersInitialState = {
+    destinos: intializeDestinosViajesState()
+};
+//fin redux init
 
 @NgModule({
   declarations: [
@@ -28,7 +51,9 @@ const routes: Routes = [
     BrowserModule,
     FormsModule,
     ReactiveFormsModule,
-    RouterModule.forRoot(routes)
+    RouterModule.forRoot(routes),
+    NgRxStoreModule.forRoot(reducers, { initialState: reducersInitialState }),
+    EffectsModule.forRoot([DestinosViajesEffects])
   ],
   providers: [
     DestinosApiClient
--- angular-whishlist/src/app/form-destino-viaje/form-destino-viaje.component.ts
+++ angular-whishlist/src/app/form-destino-viaje/form-destino-viaje.component.ts
@@ -1,6 +1,9 @@
 import { Component, EventEmitter, Output, OnInit } from '@angular/core';
 import { DestinoViaje } from './../models/destino-viaje.model';
 import { FormBuilder, FormGroup, Validators, FormControl, ValidatorFn } from '@angular/forms';
+import { fromEvent } from 'rxjs';
+import { map,filter, debounceTime, distinctUntilChanged, switchMap } from 'rxjs/operators';
+import { ajax, AjaxResponse } from 'rxjs/ajax';
 
 @Component({
   selector: 'app-form-destino-viaje',
@@ -11,6 +14,7 @@ export class FormDestinoViajeComponent implements OnInit {
   @Output() onItemAdded: EventEmitter<DestinoViaje>;
   fg:FormGroup;
   minLongitud = 5;
+  searchResults: string[];
 
   constructor(fb: FormBuilder) {
     this.onItemAdded = new EventEmitter();
@@ -38,6 +42,18 @@ export class FormDestinoViajeComponent implements OnInit {
   }
 
   ngOnInit() {
+    const elemNombre = <HTMLInputElement>document.getElementById('nombre');
+    fromEvent(elemNombre, 'input')
+      .pipe(
+        map((e: KeyboardEvent) => (e.target as HTMLInputElement).value), 
+        filter(text => text.length > 2 ),
+        debounceTime(200),
+        distinctUntilChanged(),
+        switchMap(() => ajax('/assets/datos.json'))
+      ).subscribe(ajaxResponse => {
+        console.log(ajaxResponse.response);
+        this.searchResults = ajaxResponse.response;
+      });
   }
 
   guardar(nombre:string, url:string):boolean {
