e1c9961b5796839b615a207d633381dbe99037d5
Luis Munoz <lmunoz.xyz@gmail.com>
2020-11-22 11:49:56 -0600

    mapas todo

--- angular-whishlist/src/app/components/destino-detalle/destino-detalle.component.ts
+++ angular-whishlist/src/app/components/destino-detalle/destino-detalle.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, InjectionToken, Inject } from '@angular/core';
 import { DestinosApiClient } from './../../models/destinos-api-client.model';
 import { DestinoViaje } from './../../models/destino-viaje.model';
 import { ActivatedRoute } from '@angular/router';
@@ -6,16 +6,35 @@ import { ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-destino-detalle',
   templateUrl: './destino-detalle.component.html',
-  styleUrls: ['./destino-detalle.component.css']
+  styleUrls: ['./destino-detalle.component.css'],
+  providers: [DestinosApiClient]
 })
 export class DestinoDetalleComponent implements OnInit {
-  destino:DestinoViaje;
+  destino: DestinoViaje;
+  style = {
+    sources: {
+      world: {
+        type: 'geojson',
+        data: 'https://raw.githubusercontent.com/johan/world.geo.json/master/countries.geo.json'
+      }
+    },
+    version: 8,
+    layers: [{
+      'id': 'countries',
+      'type': 'fill',
+      'source': 'world',
+      'layout': {},
+      'paint': {
+        'fill-color': '#6F788A'
+      }
+    }]
+  };
 
-  constructor(private route: ActivatedRoute, private destinosApiClient:DestinosApiClient) {}
+  constructor(private route: ActivatedRoute, private destinosApiClient: DestinosApiClient) {}
 
   ngOnInit() {
-	  let id = this.route.snapshot.paramMap.get('id');
-      this.destino = null; // this.destinosApiClient.getById(id);
+    const id = this.route.snapshot.paramMap.get('id');
+    this.destino = this.destinosApiClient.getById(id);
   }
 
 }
